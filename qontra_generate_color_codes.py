c117d3bc7a7cb60d868be5b41020244e5109de1e
Suhas Vittal <suhaskvittal@gmail.com>
2023-12-29 18:12:22 -0500

    updates to qontra_generate_...

--- qontra_generate_color_codes.py
+++ qontra_generate_color_codes.py
@@ -10,7 +10,7 @@ inputs = parse(argv)
 use_flags = ('f' in inputs)
 both_at_once = ('bac' in inputs)
 
-for d in [3, 5, 7, 9, 11, 13]:
+for d in [3, 5, 7, 9, 11, 13, 15, 17]:
     for memory in ['x', 'z']:
         filename = '%s/memory_%s_d%d.asm' % (QONTRA_ASM_WRITE_DIRECTORY, memory, d)
         asm_hexagonal_color_code(filename, d, d, memory=memory, use_flags=use_flags, both_at_once=both_at_once)
